84f0114b8660d2bd0394b5808ee37b93ce23cfa4
HasanOmar1 <r3domar1@gmail.com>
2024-09-24 15:03:01 +0300

    added

---
+++ javaScript/subtractionCalc.js
@@ -0,0 +1,104 @@
+const form = document.querySelector("form");
+const firstInput = document.querySelector("#num1");
+const secondInput = document.querySelector("#num2");
+const imgsContainer = document.querySelector(".imgs-container");
+const randomInput1 = document.querySelector(".random-1");
+const randomInput2 = document.querySelector(".random-2");
+
+randomInput1.addEventListener("click", () => {
+  let randomNumbers = Math.ceil(Math.random() * 1000);
+  firstInput.value = randomNumbers;
+});
+
+randomInput2.addEventListener("click", () => {
+  let randomNumbers = Math.ceil(Math.random() * 1000);
+  secondInput.value = randomNumbers;
+});
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function appendImg(array, i, number) {
+  if (array[i] === number) {
+    const imgElement = document.createElement("img");
+    imgsContainer.append(imgElement);
+    imgElement.src = `../numbers/${number}.jpg`;
+    imgElement.alt = number;
+  }
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  while (imgsContainer.firstChild) {
+    imgsContainer.removeChild(imgsContainer.firstChild);
+  }
+
+  const firstInputValue = parseInt(firstInput.value);
+  const secondInputValue = parseInt(secondInput.value);
+  let result;
+
+  result = subtract(firstInputValue, secondInputValue);
+
+  let num1String = firstInputValue.toString().split("");
+  let num2String = secondInputValue.toString().split("");
+  let resultToString = result.toString().split("");
+
+  for (let i = 0; i < num1String.length; i++) {
+    appendImg(num1String, i, "0");
+    appendImg(num1String, i, "1");
+    appendImg(num1String, i, "2");
+    appendImg(num1String, i, "3");
+    appendImg(num1String, i, "4");
+    appendImg(num1String, i, "5");
+    appendImg(num1String, i, "6");
+    appendImg(num1String, i, "7");
+    appendImg(num1String, i, "8");
+    appendImg(num1String, i, "9");
+  }
+
+  const imgElement = document.createElement("img");
+  imgsContainer.append(imgElement);
+  imgElement.src = `../numbers/-.jpg`;
+  imgElement.classList.add("operator");
+  imgElement.alt = "minus";
+
+  for (let i = 0; i < num2String.length; i++) {
+    appendImg(num2String, i, "0");
+    appendImg(num2String, i, "1");
+    appendImg(num2String, i, "2");
+    appendImg(num2String, i, "3");
+    appendImg(num2String, i, "4");
+    appendImg(num2String, i, "5");
+    appendImg(num2String, i, "6");
+    appendImg(num2String, i, "7");
+    appendImg(num2String, i, "8");
+    appendImg(num2String, i, "9");
+  }
+
+  const equalImg = document.createElement("img");
+  imgsContainer.append(equalImg);
+  equalImg.src = "../numbers/=.jpg";
+  equalImg.classList.add("operator");
+  equalImg.alt = "equal";
+
+  for (let i = 0; i < resultToString.length; i++) {
+    appendImg(resultToString, i, "-");
+    appendImg(resultToString, i, "0");
+    appendImg(resultToString, i, "1");
+    appendImg(resultToString, i, "2");
+    appendImg(resultToString, i, "3");
+    appendImg(resultToString, i, "4");
+    appendImg(resultToString, i, "5");
+    appendImg(resultToString, i, "6");
+    appendImg(resultToString, i, "7");
+    appendImg(resultToString, i, "8");
+    appendImg(resultToString, i, "9");
+  }
+
+  firstInput.value = "";
+  secondInput.value = "";
+}
+
+form.addEventListener("submit", handleSubmit);
---
+++ javaScript/subtractionExam.js
@@ -0,0 +1,77 @@
+const form = document.querySelector("form");
+const submitMsg = document.querySelector(".submit-msg");
+const submitFormBtn = document.querySelector("#submit-btn");
+const newExamBtn = document.querySelector("#new-exam");
+
+// 1
+let firstNum1 = document.querySelector("#first-num-1");
+let secondNum1 = document.querySelector("#second-num-1");
+let result1 = document.querySelector("#result-num-1");
+let emoji1 = document.querySelector("#emoji-1");
+let correctAnswer1 = document.querySelector(".correct-answer-1");
+firstNum1.innerText = parseInt(Math.ceil(Math.random() * 1000));
+secondNum1.innerText = parseInt(Math.ceil(Math.random() * 1000));
+
+// 2
+let firstNum2 = document.querySelector("#first-num-2");
+let secondNum2 = document.querySelector("#second-num-2");
+let result2 = document.querySelector("#result-num-2");
+let emoji2 = document.querySelector("#emoji-2");
+let correctAnswer2 = document.querySelector(".correct-answer-2");
+firstNum2.innerText = parseInt(Math.ceil(Math.random() * 1000));
+secondNum2.innerText = parseInt(Math.ceil(Math.random() * 1000));
+
+// 3
+let firstNum3 = document.querySelector("#first-num-3");
+let secondNum3 = document.querySelector("#second-num-3");
+let result3 = document.querySelector("#result-num-3");
+let emoji3 = document.querySelector("#emoji-3");
+let correctAnswer3 = document.querySelector(".correct-answer-3");
+firstNum3.innerText = parseInt(Math.ceil(Math.random() * 1000));
+secondNum3.innerText = parseInt(Math.ceil(Math.random() * 1000));
+
+// 4
+let firstNum4 = document.querySelector("#first-num-4");
+let secondNum4 = document.querySelector("#second-num-4");
+let result4 = document.querySelector("#result-num-4");
+let emoji4 = document.querySelector("#emoji-4");
+let correctAnswer4 = document.querySelector(".correct-answer-4");
+firstNum4.innerText = parseInt(Math.ceil(Math.random() * 1000));
+secondNum4.innerText = parseInt(Math.ceil(Math.random() * 1000));
+
+let numberOfCorrectAnswers = 0;
+
+function ifStatementFunction(num1, num2, result, emoji, correctAnswer) {
+  let sum = parseInt(num1.innerText) - parseInt(num2.innerText);
+  if (sum === parseInt(result.value)) {
+    emoji.innerText = "✔";
+    emoji.classList.add("correct");
+    numberOfCorrectAnswers++;
+  } else {
+    emoji.innerText = "❌";
+    correctAnswer.innerText = `The Correct Answer is : ${sum}`;
+    correctAnswer.classList.add("wrong-border");
+  }
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  ifStatementFunction(firstNum1, secondNum1, result1, emoji1, correctAnswer1);
+  ifStatementFunction(firstNum2, secondNum2, result2, emoji2, correctAnswer2);
+  ifStatementFunction(firstNum3, secondNum3, result3, emoji3, correctAnswer3);
+  ifStatementFunction(firstNum4, secondNum4, result4, emoji4, correctAnswer4);
+
+  submitMsg.innerText = `You Got ${numberOfCorrectAnswers} Correct Answers!`;
+
+  result1.disabled = true;
+  result2.disabled = true;
+  result3.disabled = true;
+  result4.disabled = true;
+
+  submitFormBtn.disabled = true;
+}
+
+form.addEventListener("submit", handleSubmit);
+newExamBtn.addEventListener("click", () => {
+  window.location.reload();
+});
